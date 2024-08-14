55e7b2a256eb20aa5cd266ff8fa2fef4758e53a9
Matthias Osswald <matthias@bespinian.io>
2024-08-14 15:32:52 +0200

    Clean up files.

--- darwin.nix
+++ darwin.nix
@@ -1,35 +1,24 @@
 { pkgs, ... }: 
 
 {
-  # List packages installed in system profile. To search by name, run:
-  # $ nix-env -qaP | grep wget
   environment.systemPackages = [
   ];
 
-  # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
-  # services.karabiner-elements.enable = true;
-  # nix.package = pkgs.nix;
 
-  # Necessary for using flakes on this system.
   nix.settings.experimental-features = "nix-command flakes";
 
-  nixpkgs.config.allowUnfree = true;
-  nixpkgs.config.allowUnfreePredicate = _: true;
+  nixpkgs = {
+    hostPlatform = "aarch64-darwin";
+    config = {
+      allowUnfree = true;
+      allowUnfreePredicate = _: true;
+    };
+  };
 
-  # Create /etc/zshrc that loads the nix-darwin environment.
-  programs.zsh.enable = true;  # default shell on catalina
-  # programs.fish.enable = true;
+  programs.zsh.enable = true;
 
-  # Set Git commit hash for darwin-version.
-  # system.configurationRevision = self.rev or self.dirtyRev or null;
-
-  # Used for backwards compatibility, please read the changelog before changing.
-  # $ darwin-rebuild changelog
   system.stateVersion = 4;
-
-  # The platform the configuration will be used on.
-  nixpkgs.hostPlatform = "aarch64-darwin";
 
   users.users.speedy = {
     name = "speedy";
--- home.nix
+++ home.nix
@@ -1,24 +1,17 @@
 { config, pkgs, ... }:
 
 {
-  home.stateVersion = "23.05"; # Please read the comment before changing.
-
   imports = [
     ./modules/aliases.nix
     (import ./modules/packages.nix { inherit pkgs; })
   ];
 
-  home.file = {
-    # Add any file-specific configuration here if needed
-  };
-
-  home.sessionVariables = {
-    # Add any session variables you want to set here
-  };
-
-  programs.home-manager.enable = true;
+  home.stateVersion = "23.05";
+  home.file = {};
+  home.sessionVariables = {};
 
   programs = {
+    home-manager.enable = true;
     alacritty = import ./modules/programs/alacritty.nix;
     git = import ./modules/programs/git.nix;
     neovim = import ./modules/programs/neovim.nix { inherit pkgs; };
