91559aa8c76967a4cfb9851abdb0cd703373456b
Matthias Osswald <matthias@busykoala.io>
2025-01-09 12:07:59 +0100

    Update ssh config.

--- modules/programs/ssh.nix
+++ modules/programs/ssh.nix
@@ -3,8 +3,17 @@
 
   extraConfig = ''
     Host *
-        UseKeychain yes
-        AddKeysToAgent yes
-        IdentityFile ~/.ssh/id_rsa
+        UseKeychain         yes
+        AddKeysToAgent      yes
+        IdentityFile        ~/.ssh/id_rsa
+
+    Host                    rootServer
+        Hostname            194.13.82.8
+        User                root
+        ProxyJump           cockpitServer
+
+    Host                    cockpitServer
+        Hostname            194.13.80.17
+        User                root
   '';
 }
